531b22fff586adc58a507540ca786490da3f45f3
João Vitor R. da Silva <joaovitor.r.silva.3@gmail.com>
2024-11-23 23:57:44 -0300

    onSelectItem Prop

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import ListGroup from "./components/ListGroup";
 
 function App() {
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
   return (
     <h1>
-      <ListGroup />
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
     </h1>
   );
 }
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,19 @@
 import { useState } from "react";
 
-function ListGroup() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+// {items: [], heading: string}
+interface Props {
+  items: string[];
+  heading: string;
+  // (item: string) => void
+  onSelectItem: (item: string) => void;
+}
 
+function ListGroup({ items, heading, onSelectItem }: Props) {
   //Hook
   const [selectedIndex, setSelectedIndex] = useState(-1);
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
       {items.length === 0 && <p>No item found!</p>}
       <ul className="list-group">
         {items.map((item, index) => (
@@ -20,6 +26,7 @@ function ListGroup() {
             key={item}
             onClick={() => {
               setSelectedIndex(index);
+              onSelectItem(item);
             }}
           >
             {item}
